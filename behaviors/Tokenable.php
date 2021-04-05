8678e15e52230459afe0f5da9c82132e306fd1f8
Trias Nur Rahman <contact@triasrahman.com>
2021-04-05 10:31:21 +0700

    Implement behavior to create personal access token

--- behaviors/Tokenable.php
+++ behaviors/Tokenable.php
@@ -1,8 +1,38 @@
 <?php namespace Octobro\OAuth2\Behaviors;
 
 use Laravel\Passport\HasApiTokens;
+use Illuminate\Container\Container;
+use Laravel\Passport\PersonalAccessTokenFactory;
 
 class Tokenable extends \October\Rain\Extension\ExtensionBase
 {
     use HasApiTokens;
+    
+    /**
+     * @var \October\Rain\Database\Model Reference to the extended model.
+     */
+    protected $model;
+
+    /**
+     * Constructor
+     * @param \October\Rain\Database\Model $model The extended model.
+     */
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    /**
+     * Create a new personal access token for the user.
+     *
+     * @param  string  $name
+     * @param  array  $scopes
+     * @return \Laravel\Passport\PersonalAccessTokenResult
+     */
+    public function createToken($name, array $scopes = [])
+    {
+        return Container::getInstance()->make(PersonalAccessTokenFactory::class)->make(
+            $this->model->getKey(), $name, $scopes
+        );
+    }
 }
