5b3a864ec597aa66ef65659cdc91a7246ae6e120
Trias Nur Rahman <contact@triasrahman.com>
2021-04-26 09:20:30 +0700

    Remove throttle middleware

--- routes.php
+++ routes.php
@@ -7,11 +7,9 @@
         'middleware' => 'cors'
         ], function() {
             
-            Route::group(['middleware' => 'throttle'], function() {    
-                Route::post('auth/access_token', 'Auth@accessToken');
-                Route::post('auth/register', 'Auth@register');
-                Route::post('auth/forgot', 'Auth@forgot');
-            });
+            Route::post('auth/access_token', 'Auth@accessToken');
+            Route::post('auth/register', 'Auth@register');
+            Route::post('auth/forgot', 'Auth@forgot');
 
             Route::group(['middleware' => 'oauth'], function() {
                 Route::get('me', 'Me@show');
